227b954c9cbee7ce9b776a666b586689910824a1
cs690creativedev <32576051+cs690creativedev@users.noreply.github.com>
2018-04-27 09:53:08 -0400

    cckk5

---
+++ 27April18-ck-project-Eve/ck-project/Models/CLSA7.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ck_project.Models
+{
+    public class CLSA7
+    {
+        public string Designer { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Open { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Sold { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Deferred { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Lost_Price { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Lost_Comp { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Lost_Other { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total_Closed { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public int Total { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0.00}")]
+        public decimal Closed_Percentage { get; set; }
+    }
+}
